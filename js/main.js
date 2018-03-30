0b9c614cc9403f9015a8c69cd138f9e807114749
Waqas Ali <waqas.abbasi@outlook.com>
2018-03-30 22:40:15 +0800

    #7 Pretty frontend (#8)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 addMachines = function(machines, machine_type, selector) {
   $.each(machines, function(key, machine){
-    status = machine.status.is_free ? 'Free' : 'Busy';
-    html_element = $('<li>').text(machine_type + ' ' + machine.id + ': ' + status);
+    html_element = $('<li>').addClass('list-group-item d-flex justify-content-between align-items-center');
+    html_element.text([machine_type, machine.id].join(' '));
+
+    badge_element = $('<span>').addClass('badge badge-pill');
+    if (machine.status.is_free) {
+        badge_element.addClass('badge-success').text('Free');
+        html_element.addClass('text-success');
+    } else {
+        badge_element.addClass('badge-danger').text('Busy');
+    }
+    html_element.append(badge_element);
+    
     $(selector).append(html_element);
   });
 };
-
 getLaundryStatus = function() {
   endpoint_url = 'https://cstc.azurewebsites.net/laundry/status';
   $.get(endpoint_url, function(response) {
